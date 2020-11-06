113c954dec882e167fd028207c66ad40151db5c7
Abdoul Aziz Kebe <abdoulazizkebe29@gmail.com>
2020-11-06 14:21:57 +0000

    first 2 deploy app

--- application/config/database.php
+++ application/config/database.php
@@ -76,10 +76,10 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
+	'username' => '',
 	'password' => '',
 	'database' => '',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => '',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
